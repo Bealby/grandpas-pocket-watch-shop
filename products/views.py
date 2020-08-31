01274f566ee0d9881ec3a28089521c82dac67a35
Bealby <mgbealby@gmail.com>
2020-08-31 13:05:52 +0000

    Added product content in veiw.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def index(request):
+    # A view to return all products and search queries
+
+    products = Product.objects.all
+    
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
