dbca1e985eaaeb22e2a31ad1eaba7686d77457f4
Bealby <mgbealby@gmail.com>
2020-09-05 10:45:54 +0000

    Completed models for checkout. Pre migration

--- checkout/models.py
+++ checkout/models.py
@@ -8,26 +8,42 @@ from products.models import Product
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=false, editable=False)
-    full_name = models.CharField(max_length=50, null=false, black=False)
-    email = models.CharField(max_length=254, null=false, black=False)
-    phone_number_number = models.CharField(max_length=20, null=false, black=False)
-    county = models.CharField(max_length=40, null=false, black=False)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.CharField(max_length=254, null=False, blank=False)
+    phone_number_number = models.CharField(max_length=20, null=False, blank=False)
+    county = models.CharField(max_length=40, null=False, blank=False)
     # Postcode not required as do not exist in every country
-    postcode = models.CharField(max_length=20, null=false, black=False)
-    town_or_city = models.CharField(max_length=40, null=false, black=False)
-    street_address1 = models.CharField(max_length=80, null=false, black=False)
-    street_address2 = models.CharField(max_length=80, null=false, black=False)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=False, blank=False)
     # County not required as do not exist in every country
-    county = models.CharField(max_length=80, null=false, black=False)
-    date = models.CharField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_length=6, decimal_places=2, null=False default=0)
-    order_total = models.DecimalField(max_length=10, decimal_places=2, null=False default=0)
-    grand_total = models.DecimalField(max_length=10, decimal_places=2, null=False default=0)
+    county = models.CharField(max_length=80, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
     def _generate_order_number(self):
     # Generate unique order number using UUID
         return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+    # Update grand total for each itme added including postage cost
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+        
+    def save(self, *args, **kwargs):
+    # Overide original save if no order number hasn't been set
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+    def __str__(self):
+        return self.order_number
+    
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
@@ -35,3 +51,10 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
+    def save(self, *args, **kwargs):
+    # Overide original save to set grand total and update order number
+        self.item_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
