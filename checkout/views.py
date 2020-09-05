5292e8665cacd1a354ecfb1fa97a9224b2e0cd5d
Bealby <mgbealby@gmail.com>
2020-09-05 13:47:49 +0000

    Completed Checkout views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    basket = request.session.get('basket', {})
+    # To prevent people from manually accessing the URL by typing /checkout
+    if not basket:
+        message.error(request, "there's nothing in your basket at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    } 
+
+    return render(request, tmeplate, context)  
+
+
+
+
