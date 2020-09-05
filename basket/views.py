146ab9735b89c5609b9e29d63a5d0d93b456df32
Bealby <mgbealby@gmail.com>
2020-09-05 04:34:01 +0000

    Merge Toasts branch with master

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +14,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     # Add product to shopping basket
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -19,6 +23,7 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
 
@@ -43,11 +48,14 @@ def remove_from_basket(request, item_id):
     # Remove item from basket
     
     try:
+        product = get_object_or_404(Product, pk=item_id) 
         basket = request.session.get('basket', {})
         basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
