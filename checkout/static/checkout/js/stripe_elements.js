d62097e9e4e161864b05c2b0e2a6c70f26f2ebf1
Bealby <mgbealby@gmail.com>
2020-09-06 21:52:43 +0000

    Styled checkout view for form and stripe. checkout.css and stripe_elements.js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,24 +5,23 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripePublicKey);
-var elements = stripe.elements();
-var style = {
+let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+let client_secret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripe_public_key);
+let elements = stripe.elements();
+let style = {
     base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
+        color: 'rgb(0, 0, 0, 1)',
+        fontFamily: '"Roboto", sans-serif',
+        fontSize: '1.2rem',
         '::placeholder': {
-            color: '#aab7c4'
+            color: 'rgb(170, 186, 196)'
         }
     },
     invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
+        color: 'rgb(220,53,69,1)',
+        iconColor: 'rgb(220,53,69,1)'
     }
 };
-var card = elements.create('card', {style: style});
+let card = elements.create('card', {style: style});
 card.mount('#card-element');
