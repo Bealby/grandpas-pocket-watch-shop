7fd26e695f05a9a28e5c01b1a3258cf160a5d6a4
Bealby <mgbealby@gmail.com>
2020-09-07 08:41:08 +0000

    Added public and secret keys to enviroment variables

--- checkout/views.py
+++ checkout/views.py
@@ -1,23 +1,44 @@
 from django.shortcuts import render, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
-from basket.contexts import bag_contents
+from basket.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     # To prevent people from manually accessing the URL by typing /checkout
     if not basket:
         message.error(request, "there's nothing in your basket at the moment")
         return redirect(reverse('products'))
 
+    # To calcualte basket total for stripe
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HC2c8IWzYaUEph7E6NjAYYXDJv2kJ9vH0ccy7jzmlGd10jYEyHNOwHGqXmwXQydkO4Vdz4B31jCNHDks7br1lK000NopfUW1c',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
