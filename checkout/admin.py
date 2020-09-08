5810fe8d288e7b4a903afe70309eccb9854d82e2
Bealby <mgbealby@gmail.com>
2020-09-08 13:43:37 +0000

    Completed webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -5,24 +5,26 @@ from .models import Order, OrderLineItem
 class OrderLineItemAdminInline(admin.TabularInline):
     model = OrderLineItem
     readonly_fields = ('lineitem_total',)
-    
-
-class OrderLineItemAdminInline(admin.TabularInline):
-    inlines = (OrderLineItemAdminInline,)
-
-    model = OrderLineItem
-    readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
-    # Read only to prevent fraud/editing reducing integrity of order
-    readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'grand_total')
-    # Fixes order of fields 
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county', 'delivery_cost', 'order_total', 'grand_total')
-    # Restrict columns that show up in the order list to selective few.
-    list_display = ('order_number', 'date', 'full_name', 'order_total', 'delivery_cost', 'grand_total')
 
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total', 'original_basket',
+                       'stripe_pid')
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total', 'original_basket',
+              'stripe_pid')
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total',)
 
     ordering = ('-date',)
 
