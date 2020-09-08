8c46a6abb75f1368246abc1feafdee8e7fee9cba
Bealby <mgbealby@gmail.com>
2020-09-08 19:33:11 +0000

    Adjust map.js and contact.html for final (basic) layout

--- static/js/map.js
+++ static/js/map.js
@@ -8,17 +8,10 @@ function initMap() {
     '<div id="content">' +
     '<div id="siteNotice">' +
     "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<h1 id="firstHeading" class="firstHeading">Grandpas Pocket Watch Shop</h1>' +
     '<div id="bodyContent">' +
     "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
     "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-    "Aboriginal people of the area. It has many springs, waterholes, " +
-    "rock caves and ancient paintings. Uluru is listed as a World " +
     "Heritage Site.</p>" +
     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
     "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
@@ -27,12 +20,12 @@ function initMap() {
     "</div>";
   const infowindow = new google.maps.InfoWindow({
     content: contentString,
-    maxWidth: 200
+    maxWidth: 400
   });
   const marker = new google.maps.Marker({
     position: uluru,
     map,
-    title: "Uluru (Ayers Rock)"
+    title: "Grandpa's Pocket Watch Shop"
   });
   marker.addListener("click", () => {
     infowindow.open(map, marker);
