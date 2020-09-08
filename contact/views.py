da07cbc035428cc58663aa78f50997bee65f1294
Bealby <mgbealby@gmail.com>
2020-09-08 16:43:56 +0000

    Added google map api. Set environment variables

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,14 @@
+import os
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 
+
 def view_contact(request):
 # A view to render contact page
+    context = {
+            'api_key': settings.GOOGLE_MAP_API_KEY,
+    }
 
     return render(request, 'contact/contact.html')
