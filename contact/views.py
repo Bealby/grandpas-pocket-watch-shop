2e946db4fdc1b9216c7d1bff42dfefe0c4509c6c
Bealby <mgbealby@gmail.com>
2020-09-08 18:11:50 +0000

    Settings for email settings/urls/views/html

--- contact/views.py
+++ contact/views.py
@@ -1,14 +1,24 @@
-import os
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.conf import settings
+from .forms import ContactForm
 
-# Create your views here.
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "contact.html", {'form': form})
 
-
-def view_contact(request):
-# A view to render contact page
-    context = {
-            'api_key': settings.GOOGLE_MAP_API_KEY,
-    }
-
-    return render(request, 'contact/contact.html')
+def successView(request):
+    return HttpResponse('Success! Thank you for your message.')
