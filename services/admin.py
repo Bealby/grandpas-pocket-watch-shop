83f986ea5133797620f712049995e6a7fd18cb57
Bealby <mgbealby@gmail.com>
2020-09-09 08:45:47 +0000

    Migrated models in services app. Set display in admin

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import AppointmentType, PocketWatch
 
-# Register your models here.
+
+class AppointmentTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class PocketWatchAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'model',
+        'type',
+        'appointment',
+        'date',
+    )
+
+    ordering = ('date',)
+
+
+admin.site.register(AppointmentType, AppointmentTypeAdmin)
+admin.site.register(PocketWatch, PocketWatchAdmin)
