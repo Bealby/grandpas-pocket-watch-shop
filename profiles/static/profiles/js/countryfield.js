27ac0050600c71708e3417d3174070daa2c0ba04
Bealby <mgbealby@gmail.com>
2020-09-12 18:36:42 +0000

    Set up order info checkout/profiles templates/views/urls. Created js for forms.py

--- profiles/static/profiles/js/countryfield.js
+++ profiles/static/profiles/js/countryfield.js
@@ -1,12 +1,12 @@
 // To ensure counrty field content is grey when not selected
 
 let countrySelected = $('#id_default_country').val();
+    // If false be #aab7c4 placement color
 if(!countrySelected) {
     $('#id_default_country').css('color', '#aab7c4');
 };
 $('#id_default_country').change(function() {
     countrySelected = $(this).val();
-    // If false be #aab7c4 placement color
     if(!countrySelected) {
         $(this).css('color', '#aab7c4');
     } else {
