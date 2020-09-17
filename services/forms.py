fcf79c341d301c55e3c92aec6ff5be067ef87b87
Bealby <mgbealby@gmail.com>
2020-09-17 19:30:53 +0000

    Added placeholders to views.py for services app

--- services/forms.py
+++ services/forms.py
@@ -21,5 +21,23 @@ class AppointmentForm(forms.ModelForm):
         watch_types = WatchType.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in watch_types]
         self.fields['watch_type'].choices = friendly_names
-        
+
+        placeholders = {
+            'name': 'Full Name',
+            'email': 'Email',
+            'appointment_type': '',
+            'watch_model': '',
+            'watch_type': '',
+            'date': 'Date/ Time?',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]}'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+
         self.fields['date'].widget.attrs['class'] = 'datepicker'
+
