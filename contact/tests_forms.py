c9d4a9e70700167cefaab29d212ac415f15fabfb
Bealby <mgbealby@gmail.com>
2020-09-18 10:26:28 +0000

    Run forms.py contact TestCase

---
+++ contact/tests_forms.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from .forms import ContactForm
+
+
+class TestContactForm(TestCase):
+    def test_contact_name_is_required(self):
+        form = ContactForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('name', form.errors.keys())
+        self.assertEqual(form.errors['name'][0], 'This field is required.')
+
+        form = ContactForm({'email': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('email', form.errors.keys())
+        self.assertEqual(form.errors['email'][0], 'This field is required.')
+
+        form = ContactForm({'message': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('message', form.errors.keys())
+        self.assertEqual(form.errors['message'][0], 'This field is required.')
+    
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = ContactForm()
+        self.assertEqual(form.Meta.fields, ['name', 'email', 'message'])
