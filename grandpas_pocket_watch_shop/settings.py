88de1111b685e233822b74c2b50922d8cf81e5cd
Bealby <mgbealby@gmail.com>
2020-09-25 12:31:21 +0000

    Corrected AWS Region in settings

--- grandpas_pocket_watch_shop/settings.py
+++ grandpas_pocket_watch_shop/settings.py
@@ -186,7 +186,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Bucket Config
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'grandpas-pocket-watch-shop'
-    AWS_S3_REGION_NAME = 'EU'
+    AWS_S3_REGION_NAME = 'eu'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
