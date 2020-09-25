0516bf9ebadd1be6f8a4ffd8d2e323b3d5e532cf
Bealby <mgbealby@gmail.com>
2020-09-25 08:25:44 +0000

    Added test_views.py for all apps

--- basket/test_views.py
+++ basket/test_views.py
@@ -4,4 +4,9 @@ class TestViews(TestCase):
     def test_basket(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
         self.assertTemplateUsed(response, 'base.html')
+        self.assertTemplateUsed(response, 'includes/mobile-header.html')
+        self.assertTemplateUsed(response, 'includes/main-header.html')
+        self.assertTemplateUsed(response, 'includes/main-nav.html')
+        self.assertTemplateUsed(response, 'includes/footer.html')
--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 class TestViews(TestCase):
-    def test_contact(self):
+    def test_profiles(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
