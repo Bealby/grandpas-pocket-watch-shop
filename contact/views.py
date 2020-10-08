d1371e4c643c0f4987e5e7b701d28369f0924223
Bealby <mgbealby@gmail.com>
2020-10-08 09:14:40 +0000

    Added padding top for angle-right icon. Added comments

--- contact/views.py
+++ contact/views.py
@@ -36,7 +36,8 @@ def contact(request):
             message = contact_form.cleaned_data['message']
             # Message sent providing Name, Email, and Message
             try:
-                send_mail(f"{name}, <{email}>", message, email, [settings.DEFAULT_FROM_EMAIL])
+                send_mail(f"{name}, <{email}>", message, email,
+                          [settings.DEFAULT_FROM_EMAIL])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('contact_success')
