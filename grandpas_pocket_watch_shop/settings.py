74560292189bd0f057221ff6a5a1e7cecd175d6e
Bealby <mgbealby@gmail.com>
2020-10-13 14:22:39 +0000

    Changed debug/ development in settings

--- grandpas_pocket_watch_shop/settings.py
+++ grandpas_pocket_watch_shop/settings.py
@@ -26,10 +26,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+
+DEBUG=1
+
+DEVELOPMENT=1
+
+debug=os.getenv('DEVELOPMENT', False)
 
 ALLOWED_HOSTS = ['grandpas-pocket-watch-shop.herokuapp.com', 'localhost']
-
 
 # Application definition
 
