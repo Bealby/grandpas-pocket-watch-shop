cd100ba81a2b6389c7c302ca316f7ce011ba9f4a
Bealby <mgbealby@gmail.com>
2020-10-14 05:56:28 +0000

    No change to urls.py

--- grandpas_pocket_watch_shop/urls.py
+++ grandpas_pocket_watch_shop/urls.py
@@ -1,5 +1,4 @@
 """grandpas_pocket_watch_shop URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
